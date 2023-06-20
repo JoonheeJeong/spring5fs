d64232da596c59f8809536a24c354fd039567714
Joonhee Jeong <jeonggoo75@gmail.com>
2023-06-20 14:51:35 +0900

    modify #1 - MemberPrint to use DateTimeFormatter
    MemberPrinter 클래스에서 DateTimeFormatter를 사용하도록 수정합니다.
    의존관계 자동 주입을 필수가 아니도록 작성하여 null이여도 작동하게 합니다.

--- sp5-chap04/src/main/java/spring/MemberPrinter.java
+++ sp5-chap04/src/main/java/spring/MemberPrinter.java
@@ -1,9 +1,24 @@
 package spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.format.DateTimeFormatter;
+
 public class MemberPrinter {
 
+    private DateTimeFormatter dateTimeFormatter;
+
     public void print(Member member) {
-        System.out.printf("회원 정보: 아이디=%d, 이메일=%s, 이름=%s, 등록일=%tF\n",
-                member.getId(), member.getEmail(), member.getName(), member.getRegisterDateTime());
+        String registerDateTime = (dateTimeFormatter != null)
+                ? dateTimeFormatter.format(member.getRegisterDateTime())
+                : String.format("%tF", member.getRegisterDateTime());
+
+        System.out.printf("회원 정보: 아이디=%d, 이메일=%s, 이름=%s, 등록일=%s\n",
+                member.getId(), member.getEmail(), member.getName(), registerDateTime);
+    }
+
+    @Autowired(required = false)
+    public void setDateTimeFormatter(DateTimeFormatter dateTimeFormatter) {
+        this.dateTimeFormatter = dateTimeFormatter;
     }
 }
